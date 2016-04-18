a2fb2df189eb62228df9d63211d8aef574367c96
jimmylin192 <linxin@gmail.com>
2016-04-17 22:53:02 -0500

    checkpoint before implementing xaiver init algorithm

--- codebases/latte/lib.py
+++ codebases/latte/lib.py
@@ -2,40 +2,41 @@
     Standard library for Latte Programming Model
 '''
 import sys
+import numpy as np
 
 
 ensemble_id_counter = 0
 neuron_id_counter = 0
 network_id_counter = 0
 def allocate_neuron_id ():
-    assigned_id = neuron_id_counter
     global neuron_id_counter
+    assigned_id = neuron_id_counter
     neuron_id_counter += 1
     return assigned_id
 def allocate_ensemble_id ():
-    assigned_id = ensemble_id_counter
     global ensemble_id_counter
+    assigned_id = ensemble_id_counter
     ensemble_id_counter += 1
     return assigned_id
 def allocate_network_id ():
-    assigned_id = network_id_counter
     global network_id_counter
+    assigned_id = network_id_counter
     network_id_counter += 1
     return assigned_id
 
 # TODO: how to xaiver initialize
-def Xaiver_init (M, N):
+def Xaiver_weights_init (dim_x, dim_y):
     
     return [[]]
 
 def add_connection (net, prev_enm, cur_enm, mappings):
     # update adjacency lists
-    for i, indices in mappings:
+    for i, indices in mappings.iteritems():
         assert 0 <= i and i < prev_enm.get_size()
         prev_enm[i].forward_adj = [ cur_enm[j] for j in indices ]
         for j in indices: cur_enm[j].backward_adj.append(prev_enm[i])
     # add weights
-    net.add_weights(prev_enm, cur_enm, mappings)
+    # net.add_weights(prev_enm, cur_enm, mappings)
     return
 
 ''' 
@@ -77,9 +78,11 @@ def FullyConnectedLayer(net, prev_enm, N, TYPE):
     M = prev_enm.get_size()
     cur_enm = Ensemble(N, TYPE)
     cur_enm.set_inputs_dim (1, M)
+    cur_enm.set_backward_adj(prev_enm)
+    prev_enm.set_forward_adj(cur_enm)
     # enforce connections
     mappings = {}
-    for i in len(M): mappings.update(i, [j for j in range(len(N))])
+    for i in range(M): mappings.update({i:[j for j in range(N)]})
     add_connection (net, prev_enm, cur_enm, mappings)
     net.add_ensemble (cur_enm)
     return cur_enm
@@ -90,49 +93,77 @@ def SoftmaxLossLayer(net, prev_enm, nLabels):
     return 
 
 class Neuron:
-    def __init__(self):
+    def __init__(self, pos_x, pos_y):
+        # management info
         self.neuron_id = allocate_ensemble_id()
-        self.inputs = [[]]
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        # data info
+        self.weights     = [[]]
+        self.inputs      = [[]]
         self.grad_inputs = [[]]
-        self.output = 0.0
+        self.output      = 0.0
         self.grad_output = 0.0
-
-        self.forward_adj = []   # forward adjacency list
+        # architecture info
+        self.forward_adj  = []   # forward adjacency list
         self.backward_adj = []  # backward adjacency list
         return 
 
     def __eq__(self, other):
         return self.neuron_id == other.neuron_id
 
+    def init_dim (self, dim_x, dim_y):
+        self.inputs      = [ [0.0] * dim_y ] * dim_x
+        self.grad_inputs = [ [0.0] * dim_y ] * dim_x
+        self.weights     = Xaiver_weights_init (dim_x, dim_y)
+
     def forward(self):
         # innder product of inputs and weights
-
+        assert len(forward_adj) > 0, "No forward adjacency element. "
+        dp_result = 0.0
+        for i in len(self.inputs):
+            for j in len(self.inputs[0]):
+                dp_result = self.weights[i][j] * self.inputs[i][j]
+        self.output = np.tanh(dp_result)
         # put output value to the inputs of next layer
-
+        for next_neuron in forward_adj:
+            next_neuron.inputs[self.pos_x, self.pos_y] = self.output
         pass
 
     def backward(self):
-
+        
         pass
 
 # TODO:
 class DataNeuron(Neuron):
-    def __init__(self):
-        pass
+    def __init__(self, pos_x, pos_y):
+        Neuron.__init__(self, pos_x, pos_y)
+
+    def forward(self):
+        # remember to load input feature to data neuron before forward propa
+        assert len(forward_adj) > 0, "No forward adjacency element. "
+        for next_neuron in forward_adj:
+            next_neuron.inputs[self.pos_x, self.pos_y] = self.output
+
+    def backward(self):
+        pass # no backward propagation for data neuron
 
 # TODO:
 class SoftmaxNeuron(Neuron):
-    def __init__(self):
-        pass
+    def __init__(self, pos_x, pos_y):
+        Neuron.__init__(self, pos_x, pos_y)
 
 class Ensemble:
     def __init__(self, N, TYPE):
         self.ensemble_id = allocate_ensemble_id()
         self.size = N
-        self.neurons = [TYPE() for i in range(len(N))]
+        # NOTE: currently only allow 1-d ensemble
+        self.neurons = [TYPE(1, i) for i in range(N)]
+        self.prev_adj_enm = None
+        self.next_adj_enm = None
         return  
 
-    def __eq__(self, other):
+    def __eq__(self, other): 
         return self.ensemble_id == other.ensemble_id
 
     def __getitem__(self, idx):
@@ -140,21 +171,15 @@ class Ensemble:
         return self.neurons[idx]
 
     def get_size(self): return self.size
-
-    def set_inputs_dim(self, nrows, ncols):
-        for neuron in self.neurons:
-            neuron.inputs = [] 
-            neuron.grad_inputs = []
-            for i in range(nrows):
-                neuron.inputs.append([0.0] * ncols)
-                neuron.grad_inputs.append([0.0] * ncols)
-        return 
+    def set_forward_adj (self, enm):  self.prev_adj_enm = enm
+    def set_backward_adj (self, enm): self.next_adj_enm = enm
+    def set_inputs_dim(self, dim_x, dim_y):
+        for neuron in self.neurons: neuron.init_dim (dim_x, dim_y)
 
 class Network:
     def __init__(self):
         self.network_id = allocate_network_id()
         self.ensembles = []
-        self.weights = {}
         self.train_features = None
         self.train_labels = None
         self.test_features = None
@@ -170,7 +195,7 @@ class Network:
     def get_ensembles(self): return self.ensembles
 
     def add_ensemble(self, enm):
-        assert isinstance(enm, Ensemble) and len(self.ensembles) >= 2
+        assert isinstance(enm, Ensemble) and len(self.ensembles) >= 1
         self.ensembles.append(enm)
 
     def set_data_ensemble(self, data_enm):
@@ -183,9 +208,11 @@ class Network:
         self.test_features = test_fea
         self.test_labels = test_labels
 
+    '''
     def add_weights (self, prev_enm, cur_enm, mappings):
         tmp_weights = Xaiver_init(prev_enm.get_size(), cur_enm.get_size())
         self.weights.update({(prev_enm, cur_enm):tmp_weights})
+    '''
 
 #class WeightedNeuron(Neuron):
 
