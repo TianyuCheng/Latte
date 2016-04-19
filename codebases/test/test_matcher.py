d22298650c61713b93e5ebaa45a2932894e36163
tycheng <skysource.tony@gmail.com>
2016-04-19 18:38:47 -0500

    ast matcher: add matchall

--- codebases/test/test_matcher.py
+++ codebases/test/test_matcher.py
@@ -7,8 +7,8 @@ import inspect, compiler
 from latte.ast_matcher import *
 from latte.templates import *
 
-if __name__ == "__main__":
-    tmpl = forloop()
+def test_for_range():
+    tmpl = template_for_range()
     ast = compiler.parse("""
 for i in range(len(inputs[0])):
     print inputs[0][i]
@@ -19,3 +19,60 @@ for i in range(len(inputs[0])):
     if matched:
         for key, value in tmpl.wildcard.iteritems():
             print "%s:\t%s" % (key, value)
+
+def test_matchall():
+    ast = compiler.parse("""
+import os, sys
+sys.path.append(os.path.abspath(".."))
+
+from latte.lib import *
+
+net = Network()
+data_enm, nLabels = LibsvmDataLayer(net, \
+                    "../../datasets/iris-scale-train.libsvm",  \
+                    "../../datasets/iris-scale-test.libsvm", 4, 3)
+ip1_enm = FullyConnectedLayer(net, data_enm, 20, Neuron)
+ip2_enm = FullyConnectedLayer(net, ip1_enm, 10, Neuron)
+label_enm = SoftmaxLossLayer(net, ip2_enm, nLabels)
+
+sgd = SGD(10, 0.1)
+solve(sgd, net)
+    """)
+    tmpl = template_Network()
+    matched = tmpl.matchall(ast)
+    print "========================="
+    print "Match:", matched
+    for match in tmpl.matches:
+        for key, value in match.iteritems():
+            print "%s:\t%s" % (key, value)
+
+    tmpl = template_FullyConnectedLayer()
+    matched = tmpl.matchall(ast)
+    print "========================="
+    print "Match:", matched
+    for match in tmpl.matches:
+        print "----------------------"
+        for key, value in match.iteritems():
+            print "%s:\t%s" % (key, value)
+
+    tmpl = template_LibsvmDataLayer()
+    matched = tmpl.matchall(ast)
+    print "========================="
+    print "Match:", matched
+    for match in tmpl.matches:
+        print "----------------------"
+        for key, value in match.iteritems():
+            print "%s:\t%s" % (key, value)
+
+    tmpl = template_SoftmaxLossLayer()
+    matched = tmpl.matchall(ast)
+    print "========================="
+    print "Match:", matched
+    for match in tmpl.matches:
+        print "----------------------"
+        for key, value in match.iteritems():
+            print "%s:\t%s" % (key, value)
+
+if __name__ == "__main__":
+    test_for_range()
+    test_matchall()
