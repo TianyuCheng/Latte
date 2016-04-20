a9d8fd83079139989d22632e38baeab03b7455ca
jimmylin192 <linxin@gmail.com>
2016-04-19 21:10:17 -0500

    2d mlp

--- codebases/test/mlp.py
+++ codebases/test/mlp.py
@@ -10,8 +10,8 @@ net = Network()
 data_enm, nLabels = LibsvmDataLayer(net, \
                     "../../datasets/iris-scale-train.libsvm",  \
                     "../../datasets/iris-scale-test.libsvm", 4, 3)
-ip1_enm = FullyConnectedLayer(net, data_enm, 20, Neuron)
-ip2_enm = FullyConnectedLayer(net, ip1_enm, 10, Neuron)
+ip1_enm = FullyConnectedLayer(net, data_enm, 20, 20, Neuron)
+ip2_enm = FullyConnectedLayer(net, ip1_enm, 10, 10, Neuron)
 label_enm = SoftmaxLossLayer(net, ip2_enm, nLabels)
 
 sgd = SGD(10, 0.1)
