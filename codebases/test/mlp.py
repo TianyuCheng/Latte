583d7c41385575e81725268574da8e98f577e896
jimmylin192 <linxin@gmail.com>
2016-04-21 17:48:32 -0500

    forward backward rewrite and simplified

--- codebases/test/mlp.py
+++ codebases/test/mlp.py
@@ -10,8 +10,8 @@ net = Network()
 data_enm = LibsvmDataLayer(net, \
                     '../../datasets/iris-scale-train.libsvm',  \
                     '../../datasets/iris-scale-test.libsvm', 1, 4)
-ip1_enm = FullyConnectedLayer(net, data_enm, 20, 20, Neuron)
-ip2_enm = FullyConnectedLayer(net, ip1_enm, 10, 10, Neuron)
+ip1_enm = FullyConnectedLayer(net, data_enm, 1, 20, Neuron)
+ip2_enm = FullyConnectedLayer(net, ip1_enm, 1, 10, Neuron)
 label_enm = SoftmaxLossLayer(net, ip2_enm, 1, 3)
 
 sgd = SGD(10, 0.1)
