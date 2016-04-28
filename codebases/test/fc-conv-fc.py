b8ba6cf72372ef937d9872cc0cb2cacf1563a80d
jimmylin192 <linxin@gmail.com>
2016-04-28 14:22:42 -0500

    fc-conv-fc test case and debug for fc-meanp-fc

--- codebases/test/fc-conv-fc.py
+++ codebases/test/fc-conv-fc.py
@@ -1,7 +1,7 @@
 '''
-    Testcase: fc-meanp-fc.py
+    Testcase: fc-conv-fc.py
        1. FC layer with tanh activation
-       2. Pooling layer with mean activation
+       2. Convolution layer with mean activation
        3. FC layer with tanh activation
        4. softmax layer
 '''
@@ -15,7 +15,7 @@ data_enm = LibsvmDataLayer(net, \
                     '../datasets/iris-scale-train.libsvm',  \
                     '../datasets/iris-scale-test.libsvm', 1, 4, 3)
 ip1_enm = FullyConnectedLayer(net, data_enm, 1, 20, FCNeuron)
-ip2_enm = PoolingLayer(net, ip1_enm, 1, 5, MeanPoolingNeuron, 1, 4)
+ip2_enm = ConvolutionLayer(net, ip1_enm, 1, 10, WeightedNeuron, 1, 10)
 ip3_enm = FullyConnectedLayer(net, ip2_enm, 1, 10, FCNeuron)
 label_enm = SoftmaxLossLayer(net, ip3_enm, 1, 3)
 
