af02c3351741628e000c4cd75776b8e1f926ce4a
Tianyu Cheng <tianyu.cheng@utexas.edu>
2016-05-05 20:44:14 -0500

    adding test generation

--- codebases/generate_tests.sh
+++ codebases/generate_tests.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 
-make clean sbatch mini mnist-fc-fc SBATCH="./sbatches/mnist-none"
-make clean sbatch mini tiling mnist-fc-fc SBATCH="./sbatches/mnist-t"
-make clean sbatch mini tiling fusion mnist-fc-fc SBATCH="./sbatches/mnist-t-f"
+make clean sbatch mini mnist-fc-fc SBATCH="mnist-none"
+make clean sbatch mini tiling mnist-fc-fc SBATCH="mnist-t"
+make clean sbatch mini tiling fusion mnist-fc-fc SBATCH="mnist-t-f"
 
-make clean sbatch mini batch mnist-fc-fc SBATCH="./sbatches/mnist-b"
-make clean sbatch mini batch tiling mnist-fc-fc SBATCH="./sbatches/mnist-b-t"
-make clean sbatch mini batch tiling fusion mnist-fc-fc SBATCH="./sbatches/mnist-b-t-f"
+make clean sbatch mini batch mnist-fc-fc SBATCH="mnist-b"
+make clean sbatch mini batch tiling mnist-fc-fc SBATCH="mnist-b-t"
+make clean sbatch mini batch tiling fusion mnist-fc-fc SBATCH="mnist-b-t-f"
