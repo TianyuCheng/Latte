03a01265730f936fa6ad14ada79709687405bbab
Tianyu Cheng <tianyu.cheng@utexas.edu>
2016-05-05 21:33:40 -0500

    update generate script

--- codebases/generate_tests.sh
+++ codebases/generate_tests.sh
@@ -2,28 +2,26 @@
 
 EXE="mnist-fc-fc"
 
-mkdir -p sbatch
+make clean sbatch mini $EXE SBATCH="mnist-none"
+mv $EXE $EXE-none
+echo "./$EXE-none" >> ./mnist-none.sbatch
 
-make clean sbatch mini $EXE SBATCH="./sbatch/mnist-none"
-mv $EXE ./sbatch/$EXE-none
-echo "./$EXE-none" >> ./sbatch/mnist-none.sbatch
+make clean sbatch mini tiling $EXE SBATCH="mnist-t"
+mv $EXE $EXE-t
+echo "./$EXE-t" >> ./mnist-t.sbatch
 
-make clean sbatch mini tiling $EXE SBATCH="./sbatch/mnist-t"
-mv $EXE ./sbatch/$EXE-t
-echo "./$EXE-t" >> ./sbatch/mnist-t.sbatch
+make clean sbatch mini tiling fusion $EXE SBATCH="mnist-t-f"
+mv $EXE $EXE-t-f
+echo "./$EXE-t-f" >> ./mnist-t-f.sbatch
 
-make clean sbatch mini tiling fusion $EXE SBATCH="./sbatch/mnist-t-f"
-mv $EXE ./sbatch/$EXE-t-f
-echo "./$EXE-t-f" >> ./sbatch/mnist-t-f.sbatch
+make clean sbatch mini batch $EXE SBATCH="mnist-b"
+mv $EXE $EXE-b
+echo "./$EXE-b" >> ./mnist-b.sbatch
 
-make clean sbatch mini batch $EXE SBATCH="./sbatch/mnist-b"
-mv $EXE ./sbatch/$EXE-b
-echo "./$EXE-b" >> ./sbatch/mnist-b.sbatch
+make clean sbatch mini batch tiling $EXE SBATCH="mnist-b-t"
+mv $EXE $EXE-b-t
+echo "./$EXE-b-t" >> ./mnist-b-t.sbatch
 
-make clean sbatch mini batch tiling $EXE SBATCH="./sbatch/mnist-b-t"
-mv $EXE ./sbatch/$EXE-b-t
-echo "./$EXE-b-t" >> ./sbatch/mnist-b-t.sbatch
-
-make clean sbatch mini batch tiling fusion $EXE SBATCH="./sbatch/mnist-b-t-f"
-mv $EXE ./sbatch/$EXE-b-t-f
-echo "./$EXE-b-t-f" >> ./sbatch/mnist-b-t-f.sbatch
+make clean sbatch mini batch tiling fusion $EXE SBATCH="mnist-b-t-f"
+mv $EXE $EXE-b-t-f
+echo "./$EXE-b-t-f" >> ./mnist-b-t-f.sbatch
