a1a7829b5799edd40dfd2476fa50b14f705805d3
jimmylin192 <linxin@gmail.com>
2016-05-06 00:19:15 -0500

    ?

--- testcases/mnist-fc-fc.py
+++ testcases/mnist-fc-fc.py
@@ -10,9 +10,10 @@ net = Network()
 data_enm = MnistDataLayer(net, \
                     '../datasets/mnist-train.csv',  \
                     '../datasets/mnist-test.csv', 28, 28, 10)
-ip1_enm = FullyConnectedLayer(net, data_enm, 100, 100, FCNeuron)
-ip2_enm = FullyConnectedLayer(net, ip1_enm, 50, 50, FCNeuron)
-label_enm = SoftmaxLossLayer(net, ip2_enm, 1, 10)
+ip1_enm = FullyConnectedLayer(net, data_enm, 50, 50, FCNeuron)
+#ip2_enm = FullyConnectedLayer(net, ip1_enm, 30, 30, FCNeuron)
+#ip3_enm = FullyConnectedLayer(net, ip2_enm, 40, 40, FCNeuron)
+label_enm = SoftmaxLossLayer(net, ip1_enm, 1, 10)
 
 sgd = SGD(3, 0.1)
 solve(sgd, net)
