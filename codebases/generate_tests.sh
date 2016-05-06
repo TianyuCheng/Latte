3c3841690e17ed370e8b016e3d0f54dea7f89c0e
Tianyu Cheng <tianyu.cheng@utexas.edu>
2016-05-06 13:31:22 -0500

    new results

--- codebases/generate_tests.sh
+++ codebases/generate_tests.sh
@@ -9,42 +9,42 @@ function generate() {
     sbatch ./${EXE}${SUFFIX}.sbatch
 } 
 
-FLAGS=""
-SUFFIX="-none"
-generate
-
-FLAGS="tiling"
-SUFFIX="-t"
-generate
+# FLAGS=""
+# SUFFIX="-none"
+# generate
+#
+# FLAGS="tiling"
+# SUFFIX="-t"
+# generate
 
 # FLAGS="tiling fusion"
 # SUFFIX="-t-f"
 # generate
 
-# FLAGS="batch"
-# SUFFIX="-b"
+FLAGS="batch"
+SUFFIX="-b"
+generate
+
+FLAGS="batch tiling"
+SUFFIX="-b-t"
+generate
+
+# # FLAGS="batch tiling fusion"
+# # SUFFIX="-b-t-f"
+# # generate
+#
+# FLAGS="mkl"
+# SUFFIX="-m"
 # generate
 #
-# FLAGS="batch tiling"
-# SUFFIX="-b-t"
+# FLAGS="mkl tiling"
+# SUFFIX="-m-t"
 # generate
-
-# FLAGS="batch tiling fusion"
-# SUFFIX="-b-t-f"
-# generate
-
-FLAGS="mkl"
-SUFFIX="-m"
-generate
-
-FLAGS="mkl tiling"
-SUFFIX="-m-t"
-generate
-
-# FLAGS="mkl tiling fusion"
-# SUFFIX="-m-t-f"
-# generate
-
+#
+# # FLAGS="mkl tiling fusion"
+# # SUFFIX="-m-t-f"
+# # generate
+#
 # FLAGS="mkl batch"
 # SUFFIX="-m-b"
 # generate
@@ -53,6 +53,6 @@ generate
 # SUFFIX="-m-b-t"
 # generate
 
-# FLAGS="mkl batch tiling fusion"
-# SUFFIX="-m-b-t-f"
-# generate
+# # FLAGS="mkl batch tiling fusion"
+# # SUFFIX="-m-b-t-f"
+# # generate
