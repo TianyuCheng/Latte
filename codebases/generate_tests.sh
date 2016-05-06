77861a2ce4181f2788948299c81d51bded3296cd
Tianyu Cheng <tianyu.cheng@utexas.edu>
2016-05-05 23:38:02 -0500

    remove fusion tests

--- codebases/generate_tests.sh
+++ codebases/generate_tests.sh
@@ -17,9 +17,9 @@ FLAGS="tiling"
 SUFFIX="-t"
 generate
 
-FLAGS="tiling fusion"
-SUFFIX="-t-f"
-generate
+# FLAGS="tiling fusion"
+# SUFFIX="-t-f"
+# generate
 
 FLAGS="batch"
 SUFFIX="-b"
@@ -29,9 +29,9 @@ FLAGS="batch tiling"
 SUFFIX="-b-t"
 generate
 
-FLAGS="batch tiling fusion"
-SUFFIX="-b-t-f"
-generate
+# FLAGS="batch tiling fusion"
+# SUFFIX="-b-t-f"
+# generate
 
 FLAGS="mkl"
 SUFFIX="-m"
@@ -41,9 +41,9 @@ FLAGS="mkl tiling"
 SUFFIX="-m-t"
 generate
 
-FLAGS="mkl tiling fusion"
-SUFFIX="-m-t-f"
-generate
+# FLAGS="mkl tiling fusion"
+# SUFFIX="-m-t-f"
+# generate
 
 FLAGS="mkl batch"
 SUFFIX="-m-b"
@@ -53,6 +53,6 @@ FLAGS="mkl batch tiling"
 SUFFIX="-m-b-t"
 generate
 
-FLAGS="mkl batch tiling fusion"
-SUFFIX="-m-b-t-f"
-generate
+# FLAGS="mkl batch tiling fusion"
+# SUFFIX="-m-b-t-f"
+# generate
