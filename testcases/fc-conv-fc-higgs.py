a6b922156f7136e325d77d9ce7050765896d945a
Loc Hoang <lhoang@login2.stampede.tacc.utexas.edu>
2016-05-31 19:36:51 -0500

    added broken test

---
+++ testcases/fc-conv-fc-higgs.py
@@ -0,0 +1,24 @@
+'''
+    Created by Loc after the semester
+    Testcase: fc-conv-fc-higgs.py
+       1. FC layer with tanh activation
+       2. Convolution layer with mean activation
+       3. FC layer with tanh activation
+       4. softmax layer
+'''
+import os, sys
+sys.path.append(os.path.abspath(".."))
+
+from latte.lib import *
+
+net = Network()
+data_enm = LibsvmDataLayer(net,\
+                    '../datasets/higgs-train.libsvm',\
+                    '../datasets/higgs-test.libsvm', 1, 28, 2)
+ip1_enm = FullyConnectedLayer(net, data_enm, 1, 40, FCNeuron)
+ip2_enm = ConvolutionLayer(net, ip1_enm, 1, 40, WeightedNeuron, 1, 10)
+ip3_enm = FullyConnectedLayer(net, ip2_enm, 1, 10, FCNeuron)
+label_enm = SoftmaxLossLayer(net, ip3_enm, 1, 3)
+
+sgd = SGD(100, 0.1)
+solve(sgd, net)
